f698d52fa88f49e5403d0e0141be369f6b48f1cc
Jackson Kaplan <JwyattK@gmail.com>
2024-02-03 20:01:04 -0800

    cache tokens

--- playlistr/secrets.py
+++ playlistr/secrets.py
@@ -3,14 +3,24 @@ import json
 
 class Secrets:
     secrets: dict = None
+    secrets_file = "secrets.json"
 
     @classmethod
     def get(cls):
         if cls.secrets == None:
-            with open("secrets.json") as f:
+            with open(cls.secrets_file) as f:
                 cls.secrets = json.load(f)
         return cls.secrets
+
+    @classmethod
+    def set(cls, secrets_output):
+        with open(cls.secrets_file, "w") as f:
+            json.dump(secrets_output, f)
 
 
 def get():
     return Secrets.get()
+
+
+def set(secrets_output):
+    Secrets.set(secrets_output)
--- playlistr/spotify.py
+++ playlistr/spotify.py
@@ -19,45 +19,55 @@ class Spotify(Service):
             "playlist-modify-public ",
         ]
         self.ready: bool = False
-        self.auth_code: str = ""
         self.access_token: str = ""
         self.refresh_token: str = ""
+        self.user_id: str = ""
 
+        has_tokens = True
         try:
             self.access_token = secrets.get()["spotify"]["access_token"]
-        except KeyError:
-            print("No pre-existing access token available for Spotify.")
-
-        try:
             self.refresh_token = secrets.get()["spotify"]["refresh_token"]
         except KeyError:
-            print("No pre-existing refresh token available for Spotify.")
+            print("Missing access or refresh tokens in secrets.json")
+            has_tokens = False
 
-        handler = PlaylistrAuthCallbackHandler(self.auth_url())
-        self.auth_code = handler.get_auth_params()["code"]
-        self.get_token()
+        # # if we can get the user id successfully with our cached tokens, we're set.
+        if has_tokens and self.get_user_id():
+            return
 
-    def auth_url(self):
-        params = {
-            "client_id": self.client_id,
-            "response_type": "code",
-            "redirect_uri": self.redirect_uri(),
-            "scope": " ".join(self.scope),
-        }
-        headers = {}
-        req = r.Request(
-            "GET",
-            self.auth_url_base(),
-            params=params,
-            headers=headers,
-        )
+        if has_tokens:
+            self.refresh_access_token()
+        else:
+            self.get_token()
 
-        return req.prepare().url
+        got_id = self.get_user_id()
+        assert got_id
+
+    def get_user_id(self):
+        res = r.get(self.api_url_base() + "/me", headers=self.auth_headers())
+        if res.status_code != 200:
+            return False
+
+        self.user_id = res.json()["id"]
+
+        return True
+
+    def update_tokens(self, access_token: str, refresh_token: str):
+        self.access_token = access_token
+        self.refresh_token = refresh_token
+        secrets_json = secrets.get()
+        secrets_json["spotify"]["access_token"] = self.access_token
+        secrets_json["spotify"]["refresh_token"] = self.refresh_token
+        secrets.set(secrets_json)
 
     def get_token(self):
+
+        handler = PlaylistrAuthCallbackHandler(self.auth_url())
+        auth_code = handler.get_auth_params()["code"]
+
         params = {
             "grant_type": "authorization_code",
-            "code": self.auth_code,
+            "code": auth_code,
             "redirect_uri": self.redirect_uri(),
         }
         auth = f"{self.client_id}:{self.client_secret}"
@@ -70,8 +80,24 @@ class Spotify(Service):
         res = r.post(self.token_url_base(), params=params, headers=headers)
         res = res.json()
 
-        self.access_token = res["access_token"]
-        self.refresh_token = res["refresh_token"]
+        self.update_tokens(res["access_token"], res["refresh_token"])
+
+    def refresh_access_token(self):
+        params = {
+            "grant_type": "refresh_token",
+            "refresh_token": self.refresh_token,
+        }
+        auth = f"{self.client_id}:{self.client_secret}"
+        auth = base64.b64encode(auth.encode("ascii")).decode("ascii")
+        headers = {
+            "Authorization": f"Basic {auth}",
+            "Content-Type": "application/x-www-form-urlencoded",
+        }
+
+        res = r.post(self.token_url_base(), params=params, headers=headers)
+        res = res.json()
+
+        self.update_tokens(res["access_token"], self.refresh_token)
 
     def get_tracks(self):
         api_url = self.api_url_base() + "/me/tracks"
@@ -151,6 +177,23 @@ class Spotify(Service):
 
     def auth_headers(self):
         return {"Authorization": f"Bearer {self.access_token}"}
+
+    def auth_url(self):
+        params = {
+            "client_id": self.client_id,
+            "response_type": "code",
+            "redirect_uri": self.redirect_uri(),
+            "scope": " ".join(self.scope),
+        }
+        headers = {}
+        req = r.Request(
+            "GET",
+            self.auth_url_base(),
+            params=params,
+            headers=headers,
+        )
+
+        return req.prepare().url
 
     @staticmethod
     def api_url_base():
