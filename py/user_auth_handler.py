2d1a522bf1240dedbb07d9e1fa385c3ea3551d2a
Jackson Kaplan <JwyattK@gmail.com>
2024-03-16 13:04:38 -0700

    user auth handler cleanup/refactor

--- py/user_auth_handler.py
+++ py/user_auth_handler.py
@@ -18,7 +18,7 @@ class UserAuthHTTPServer(HTTPServer):
 
 class UserAuthHTTPRequestHandlerBase(BaseHTTPRequestHandler):
 
-    server: UserAuthHTTPServer  # annotate the pre-existing server var
+    auth_server: UserAuthHTTPServer  # annotate the pre-existing server var
 
     # use this to decorate handler methods
     # be sure to do server.event.set() when you're totally done. @todo that's stupid, do nested decorators or something
@@ -26,9 +26,9 @@ class UserAuthHTTPRequestHandlerBase(BaseHTTPRequestHandler):
     @staticmethod
     def do_GET_decorator(func):
         def self_wrapper(self: UserAuthHTTPRequestHandlerBase):
-            self.server.parsed_path = parse.urlparse(self.path)
-            self.server.query_dict = dict(
-                parse.parse_qsl(self.server.parsed_path.query)
+            self.auth_server.parsed_path = parse.urlparse(self.path)
+            self.auth_server.query_dict = dict(
+                parse.parse_qsl(self.auth_server.parsed_path.query)
             )
             func(self)
 
@@ -37,22 +37,25 @@ class UserAuthHTTPRequestHandlerBase(BaseHTTPRequestHandler):
     def return_successfully(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
-        print(self.server.query_dict)
+        print(self.auth_server.query_dict)
         self.end_headers()
         with open("html/success.html") as f:
             self.wfile.write(f.read().encode())
-        self.server.event.set()
+        self.auth_server.event.set()
 
 
 class UserAuthHandler:
+
+    auth_server: UserAuthHTTPServer
+
     # constructor is non-blocking.
-    def __init__(self, auth_url: str, handler_type) -> None:
-        self.server: UserAuthHTTPServer = UserAuthHTTPServer(
-            ("localhost", 8005), handler_type
+    def __init__(self, auth_url: str, request_handler_type) -> None:
+        self.auth_server  = UserAuthHTTPServer(
+            ("localhost", 8005), request_handler_type
         )
-        self.server.event: threading.Event = threading.Event()
+        self.auth_server.event = threading.Event()
         self.server_thread: threading.Thread = threading.Thread(
-            None, target=self.server.serve_forever, daemon=True
+            None, target=self.auth_server.serve_forever, daemon=True
         )
         self.server_thread.start()
 
@@ -61,11 +64,11 @@ class UserAuthHandler:
     # this method is blocking, and will not return until the user has authed
     def get_auth_params(self):
         # wait for auth to finish
-        self.server.event.wait()
+        self.auth_server.event.wait()
 
         # shut down server, kill server thread
-        self.server.shutdown()
+        self.auth_server.shutdown()
         self.server_thread.join()
 
         # return params
-        return self.server.query_dict
+        return self.auth_server.query_dict
